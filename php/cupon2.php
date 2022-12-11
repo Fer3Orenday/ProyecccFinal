379c890f88ba30b8aebe0a13103c5d894fba313d
acdlpg <gallegoscarolina61@gmail.com>
2022-12-10 23:08:48 -0600

    cupones

---
+++ php/cupon2.php
@@ -0,0 +1,49 @@
+<?php
+include ('encabezado.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./css/cupon.css">
+
+</head>
+<body style="background: #D1F2EB;">
+
+    
+</body>
+</html>
+
+<br>
+<br>
+
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">CODIGO DE CUPON ES:</h1>
+    <p class="lead">19030758021102</p>
+  </div>
+</div>
+
+<br>
+<br>
+
+    <div class="cup">
+      <img src="../imagenes/2.png" style="width:1000px;height:1000px; align=left;" alt="...">
+     
+    </div>
+  </div>
+<br>
+<br>
+
+
+
+<?php
+
+include 'pieDePagina.php';
+
+?>
