d80b45f1beaaa4136afb963d7c5aa0086a8ef0a6
josueDanielpp <parradavalosj@gmail.com>
2022-12-12 19:48:36 -0600

    este es el final commit

--- php/conexion.php
+++ php/conexion.php
@@ -3,8 +3,8 @@ $servidor = 'localhost';
 $cuenta = 'root';
 $password = '';
 $bd = 'tienda';
-
 $conexion = new mysqli($servidor,$cuenta,$password,$bd);
+$conexionaux = new mysqli($servidor,$cuenta,$password,$bd);
 if ($conexion->connect_errno) {
   die('Error en la conexion');
 }
--- php/tienda.php
+++ php/tienda.php
@@ -5,12 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
+    <link rel="icon" type="image/x-icon" href="../imagenes/favicon.png">
     <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dangrek&display=swap" rel="stylesheet">
     <style>
         .imagen:hover {filter: opacity(.5);}
         .imagen{
             width: 300px;
             height: 300px;
+        }
+        .tiendaa{
+            font-family: 'Dangrek', cursive;
+            font-size: 20px; padding-left: 20px;
+            background-color: rgb(217, 217, 235);
+
         }
     </style>
 </head>
@@ -45,22 +53,32 @@ if (isset($_REQUEST['cam'])){
     
 
 }  
-  include_once("encabezado.php");
+if(empty($_SESSION['usuario'])) {
+    include_once("encabezado.php");
+}else{
+    if($_SESSION['usuario']=="Admin"){
+        include_once("encabezadoAdmin.php");
+    }else{
+        include_once("encabezado.php");
+    }
+}
+
+  
   
 ?>
 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-        <select name="muestra" id="">
-                <option value="todos">todos</option>
+        <select name="muestra"  >
+                <option style="font-family: 'Dangrek', cursive; font-size: 20px; padding-left: 20px; background-color: rgb(217, 217, 235);" value="todos">todos</option>
                 <?php
                 $con="SELECT categoria from productos GROUP by categoria";
                 $resultado = $conexion->query($con);
                 while ($fila = $resultado->fetch_assoc()) {
-                    echo "<option  value=".$fila["categoria"].">".$fila["categoria"]."</option>";          
+                    echo "<option  class='tiendaa' value=".$fila["categoria"].">".$fila["categoria"]."</option>";          
                 }
                 ?> 
         </select>  
-        <input type="submit" value="Buscar">
+        <input style="background-color: rgb(171, 171, 218) ; padding-left: 5px; padding-right: 5px; border-radius: 20px;" type="submit" value="Buscar">
 </form>
 <section class="container">
         <div class="products">
@@ -78,22 +96,50 @@ if (isset($_REQUEST['cam'])){
                 }
                 $resultado = $conexion->query($con);
                 while ($fila = $resultado->fetch_assoc()) {
-                    echo '
-                    <form action="'.$_SERVER["PHP_SELF"].'" method="post"  enctype="multipart/form-data">
-                       <div class="carts">
-                            <div>
-                                <input type="hidden" name="id" id="id" placeholder="id" value="'.$fila['idProd'].'"  >
-                                <input type="hidden" name="precio" id="precio" placeholder="precio" value="'.$fila['precio'].'"  >
-                                <img class="imagen" src="../imagenes/'.$fila["imagen"].'" alt="">
-                                <p><span>'.$fila["precio"].'</span>$</p>
+                    $sql = 'select descuento from productos where idProd='.$fila['idProd'];
+                    $hola = $conexionaux->query($sql);
+                    //echo $sql;
+                    while ($fil = $hola->fetch_assoc()) {
+                    if($fil['descuento']==0){
+                        echo '
+                        <form action="'.$_SERVER["PHP_SELF"].'" method="post"  enctype="multipart/form-data">
+                           <div class="carts">
+                                <div>
+                                    <input type="hidden" name="id" id="id" placeholder="id" value="'.$fila['idProd'].'"  >
+                                    <input type="hidden" name="precio" id="precio" placeholder="precio" value="'.$fila['precio'].'"  >
+                                    <img class="imagen" src="../imagenes/'.$fila["imagen"].'" alt="">
+                                    <p><span>'.$fila["precio"].'</span>$</p>
+                                </div>
+                                <p class="title"> NOMBRE: '.$fila["nombre"]." </br> DESCRIPCION: ".$fila["descripcion"]."</br>  EXISTENCIA:".$fila["existencia"].'</p>
+                                <button type="submit" name="cam" class="btn btn-success">/ (agregar al carrito)</button>
+                                
                             </div>
-                            <p class="title"> NOMBRE: '.$fila["nombre"]." </br> DESCRIPCION: ".$fila["descripcion"]."</br>  EXISTENCIA:".$fila["existencia"].'</p>
-                            <button type="submit" name="cam" class="btn btn-success">/ (agregar al carrito)</button>
-                            
-                        </div>
-                     </form>   
-                   
-                       ';
+                         </form>   
+                       
+                           ';
+                    }else{
+                        $descuento=$fil['descuento'];
+                        $resta=($fila['precio']*$descuento)/100;
+                        $nuevo_precio=$fila['precio']-$resta;
+                        echo '
+                        <form action="'.$_SERVER["PHP_SELF"].'" method="post"  enctype="multipart/form-data">
+                           <div class="carts">
+                                <div>
+                                    <input type="hidden" name="id" id="id" placeholder="id" value="'.$fila['idProd'].'"  >
+                                    <input type="hidden" name="precio" id="precio" placeholder="precio" value="'.$nuevo_precio.'"  >
+                                    <img class="imagen" src="../imagenes/'.$fila["imagen"].'" alt="">
+                                    <p><span>'."Precio Antes: ".$fila["precio"]."<br>Precio Nuevo: ".$nuevo_precio.'</span>$</p>
+                                </div>
+                                <p class="title"> NOMBRE: '.$fila["nombre"]." </br> DESCRIPCION: ".$fila["descripcion"]."</br>  EXISTENCIA:".$fila["existencia"].'</p>
+                                <button type="submit" name="cam" class="btn btn-success">/ (agregar al carrito)</button>
+                                
+                            </div>
+                         </form>   
+                       
+                           ';
+
+                    }
+                }
                     
                        
                 }
